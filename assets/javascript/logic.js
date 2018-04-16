5ec66b0b20c78ae598fd2fdfeb8eda3539ea68be
madsabate <meynard.anthony.sabate@gmail.com>
2018-04-16 16:09:42 -0400

    Added google maps functionality, cleaned up gallery

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -25,7 +25,7 @@ $(document).ready(function(){
 
 window.sr = ScrollReveal();
 
-// Parallax, Landing content
+// Parallax images, Landing content
 sr.reveal('.parallax', { origin: 'top'});
 sr.reveal('.landingContent', { origin: 'left'}, 100);
 
@@ -36,5 +36,28 @@ sr.reveal('.aboutImg', {duration: 1000}, 200);
 //Completed projects
 sr.reveal('.completedProjects', {origin: 'left'}, 100);
 
+//location
+sr.reveal('#location', {origin: 'left'}, 100);
+
 //Contact
 sr.reveal('.contactUs', {origin: 'left'}, 100);
+
+//Google Map API
+function initMap() {
+    var uluru = {lat: 40.7194581, lng: -74.1871191};
+    var map = new google.maps.Map(document.getElementById('map'), {
+        zoom: 15,
+        center: uluru,
+    });
+    var marker = new google.maps.Marker ({
+        position: uluru,
+        map: map,
+        title: '59-61 Poinier Street, Newark NJ 07144'
+    });
+
+    var link = 'https://www.google.com/maps/place/59+Poinier+St+%2361,+Newark,+NJ+07114/@40.7193462,-74.1882161,17z/data=!3m1!4b1!4m5!3m4!1s0x89c253117dd70c23:0xce92b35656e74785!8m2!3d40.7193422!4d-74.1860274'
+
+    marker.addListener('click', function () {
+        window.open(link, '_blank');
+    });
+}
